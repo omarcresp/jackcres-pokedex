ab91f5cd14baa30c19250697dc58f602bc08425d
omar <crespomerchano@gmail.com>
2020-01-07 00:37:59 -0500

    PD-2 List pokedexs

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { PokedexService } from 'src/app/core/services/pokedex/pokedex.service';
+import { IPokedexResult } from 'src/app/core/interfaces/pokedex/pokedex';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,13 @@ import { PokedexService } from 'src/app/core/services/pokedex/pokedex.service';
 })
 export class HomeComponent {
 
+  public pokedexs: IPokedexResult[];
+
   constructor(
     private pokedexService: PokedexService,
   ) {
     this.pokedexService.getAll()
-      .subscribe();
+      .subscribe((d) => this.pokedexs = d.results);
   }
 
 }
